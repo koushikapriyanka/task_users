8c85946fdeabeeb966c0451c8e1ed831b91ebf09
Koushika Priyanka <priyankapaidi@gmail.com>
2017-09-04 23:50:25 +0530

    Removing ES related code

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,25 +1,8 @@
 class Task < ApplicationRecord
-	include Elasticsearch::Model
-   	include Elasticsearch::Model::Callbacks
     belongs_to :user 
-	# mappings dynamic: 'false' do
- #     indexes :title, type: 'string'
- #   end
   after_save :task_completion_check?
   validates :status, :inclusion => {:in => [1,2,3,4,5,6]}
-   def as_indexed_json(options={})
-  {
-    "id" => id,
-    "title" => name.downcase,
-    "status" => status,
-    "user_id" => user_id
-  }
-  end  
-
-  def self.search(query)
-    return Task.__elasticsearch__.search(query).results.as_json
-  end
-
+    
   def status_name
     name = {1 => "New", 2 => "In Progress", 3 => "Completed", 4 => "Expired", 5 => "Pending Approved", 6 => "Completed and Approved "}
     name[self.status]
